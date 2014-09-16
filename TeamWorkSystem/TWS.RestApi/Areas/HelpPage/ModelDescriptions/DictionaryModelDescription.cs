d75d3f52cebe4f1bae85848624fe6fb013721620
valentin-lozev <valentin.zh.lozev@gmail.com>
2014-09-16 20:13:45 +0300

    Created project structure

---
+++ TeamWorkSystem/TWS.RestApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TWS.RestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TeamWorkSystem/TWS.RestApi/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TWS.RestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
