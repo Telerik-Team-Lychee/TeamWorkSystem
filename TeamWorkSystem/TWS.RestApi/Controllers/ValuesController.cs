29884e255042343d6b3666f37ad7141eb5574d7b
Dimitar Kostov <d.kostov.dev@gmail.com>
2014-09-17 12:11:44 +0300

    Cross origin

--- TeamWorkSystem/TWS.RestApi/Controllers/ValuesController.cs
+++ TeamWorkSystem/TWS.RestApi/Controllers/ValuesController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using TWS.Data;
 
 namespace TWS.RestApi.Controllers
 {
-	
+
+    [EnableCors("*", "*", "*")]
 	public class ValuesController : ApiController
 	{
 		// GET api/values
