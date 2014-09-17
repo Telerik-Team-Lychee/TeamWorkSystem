cbb40a01cc53ad293f4c510fe791d15be0325172
Lyubomir Ganchev <luboganchev91@gmail.com>
2014-09-17 12:36:51 -0700

    Adding some extra services about the Teamwork for getting data for resources, users, admins.

--- TeamWorkSystem/TWS.RestApi/Controllers/TeamWorkController.cs
+++ TeamWorkSystem/TWS.RestApi/Controllers/TeamWorkController.cs
@@ -1,100 +1,178 @@
 ﻿namespace TWS.RestApi.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
-    using System.Web.Http;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+	using System.Web.Http;
 
-    using Microsoft.AspNet.Identity;
+	using Microsoft.AspNet.Identity;
 
-    using TWS.Data;
-    using TWS.Models;
-    using TWS.RestApi.Infrastructure;
-    using TWS.RestApi.Models;
+	using TWS.Data;
+	using TWS.Models;
+	using TWS.RestApi.Infrastructure;
+	using TWS.RestApi.Models;
 
-    public class TeamWorkController : BaseApiController
-    {
-        private IUserIdProvider userIdProvider;
+	public class TeamWorkController : BaseApiController
+	{
+		private IUserIdProvider userIdProvider;
 
-        public TeamWorkController(ITwsData data, IUserIdProvider userIdProvider)
-            : base(data)
-        {
-            this.userIdProvider = userIdProvider;
-        }
+		public TeamWorkController(ITwsData data, IUserIdProvider userIdProvider)
+			: base(data)
+		{
+			this.userIdProvider = userIdProvider;
+		}
 
-        [HttpGet]
-        public IQueryable<TeamworkModel> All()
-        {
-            var teamworks = this.data
-                .TeamWorks
-                .All()
-                .Select(TeamworkModel.FromTeamwork);
+		[HttpGet]
+		public IQueryable<TeamworkModel> All()
+		{
+			var teamworks = this.data
+				.TeamWorks
+				.All()
+				.Select(TeamworkModel.FromTeamwork);
 
-            return teamworks;
-        }
+			return teamworks;
+		}
 
-        [HttpGet]
-        public IHttpActionResult ById(int id)
-        {
-            var teamwork = this.GetCurrentTeamworkModel(id);
-            if (teamwork == null)
-            {
-                return BadRequest("Teamwork does not exist - invalid id");
-            }
+		[HttpGet]
+		public IHttpActionResult ById(int id)
+		{
+			var teamwork = this.GetCurrentTeamworkModel(id);
+			if (teamwork == null)
+			{
+				return BadRequest("Teamwork does not exist - invalid id");
+			}
 
-            return Ok(teamwork);
-        }
+			return Ok(teamwork);
+		}
 
-        [HttpPost]
-        public IHttpActionResult Create(TeamworkModel teamWork)
-        {
-            if (!this.ModelState.IsValid)
-            {
-                return BadRequest(this.ModelState);
-            }
+		/// <summary>
+		/// Get all users that participate in the current teamwork
+		/// </summary>
+		/// <param name="id">Id of the specific teamwork</param>
+		/// <returns>Collection of all contributors</returns>
+		[HttpGet]
+		public IHttpActionResult GetAllContributersById(int id)
+		{
+			var teamwork = this.data.TeamWorks.Find(id);
+			if (teamwork == null)
+			{
+				return BadRequest("Teamwork does not exist - invalid id");
+			}
 
-            var currentUserId = this.userIdProvider.GetUserId();
+			var teamworkUsers = teamwork
+				.Users
+				.Select(tw =>
+					new
+					{
+						Id = tw.Id,
+						UserName = tw.User.UserName,
+						FirstName = tw.User.FirstName,
+						LastName = tw.User.LastName,
+						Email = tw.User.Email,
+						IsOnline = tw.User.IsOnline
+					});
 
-            var newTeamwork = new TeamWork()
-            {
-                Name = teamWork.Name,
-                Description = teamWork.Description,
-                GitHubLink = teamWork.GitHubLink,
-                EndDate = teamWork.EndDate,
-                Category = teamWork.Category
-            };
+			return Ok(teamworkUsers);
+		}
 
-            newTeamwork.Users.Add(new UsersTeamWorks() { Id = currentUserId, IsAdmin = true });
-            this.data.TeamWorks.Add(newTeamwork);
-            this.data.SaveChanges();
+		/// <summary>
+		/// Get all admins in the current teamwork.
+		/// </summary>
+		/// <param name="id">Id for specific teamwork</param>
+		/// <returns>Collection of admins</returns>
+		[HttpGet]
+		public IHttpActionResult GetTeamWorkAdminsById(int id)
+		{
+			var teamwork = this.data.TeamWorks.Find(id);
+			if (teamwork == null)
+			{
+				return BadRequest("Teamwork does not exist - invalid id");
+			}
 
-            teamWork.Id = newTeamwork.Id;
-            return Ok(teamWork);
-        }
+			var teamworkAdmins = teamwork
+				.Users
+				.Where(tw => tw.IsAdmin);
 
-        [HttpDelete]
-        public IHttpActionResult Delete(int id)
-        {
-            var existingTeamwork = this.data.TeamWorks.Find(id);
-            if (existingTeamwork == null)
-            {
-                return BadRequest("Teamwork does not exist - invalid id");
-            }
+			return Ok(teamworkAdmins);
+		}
 
-            this.data.TeamWorks.Delete(existingTeamwork);
-            this.data.SaveChanges();
+		/// <summary>
+		/// Get all resource data by given teamwork id
+		/// </summary>
+		/// <param name="id">Id of the teamwork</param>
+		/// <returns>Collection of resources for the teamwork</returns>
+		[HttpGet]
+		public IHttpActionResult GetAllResourcesById(int id)
+		{
+			var teamwork = this.data.TeamWorks.Find(id);
+			if (teamwork == null)
+			{
+				return BadRequest("Teamwork does not exist - invalid id");
+			}
 
-            return Ok();
-        }
+			var resources = teamwork
+				.Resources
+				.Select(res =>
+					new
+					{
+						ResourceName = res.Name,
+						UploadedBy = res.UploadedBy.UserName,
+						Data = res.Data,
+					});
 
-        private TeamworkModel GetCurrentTeamworkModel(int id)
-        {
-            return this.data.TeamWorks
-                .All()
-                .Where(t => t.Id == id)
-                .Select(TeamworkModel.FromTeamwork)
-                .FirstOrDefault();
-        }
-    }
+			return Ok(resources);
+		}
+
+		[HttpPost]
+		public IHttpActionResult Create(TeamworkModel teamWork)
+		{
+			if (!this.ModelState.IsValid)
+			{
+				return BadRequest(this.ModelState);
+			}
+
+			var currentUserId = this.userIdProvider.GetUserId();
+
+			var newTeamwork = new TeamWork()
+			{
+				Name = teamWork.Name,
+				Description = teamWork.Description,
+				GitHubLink = teamWork.GitHubLink,
+				EndDate = teamWork.EndDate,
+				Category = teamWork.Category
+			};
+
+			newTeamwork.Users.Add(new UsersTeamWorks() { Id = currentUserId, IsAdmin = true });
+			this.data.TeamWorks.Add(newTeamwork);
+			this.data.SaveChanges();
+
+			teamWork.Id = newTeamwork.Id;
+			return Ok(teamWork);
+		}
+
+		[HttpDelete]
+		public IHttpActionResult Delete(int id)
+		{
+			var existingTeamwork = this.data.TeamWorks.Find(id);
+			if (existingTeamwork == null)
+			{
+				return BadRequest("Teamwork does not exist - invalid id");
+			}
+
+			this.data.TeamWorks.Delete(existingTeamwork);
+			this.data.SaveChanges();
+
+			return Ok();
+		}
+
+		private TeamworkModel GetCurrentTeamworkModel(int id)
+		{
+			return this.data.TeamWorks
+				.All()
+				.Where(t => t.Id == id)
+				.Select(TeamworkModel.FromTeamwork)
+				.FirstOrDefault();
+		}
+	}
 }
