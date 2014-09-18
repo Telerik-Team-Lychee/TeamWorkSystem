9c0f6b13b2a3c9f9de60ce0c1e1384de01012ea7
valentin-lozev <valentin.zh.lozev@gmail.com>
2014-09-18 17:55:42 +0300

    Merge

--- TeamWorkSystem/TWS.RestApi/Models/MessageModel.cs
+++ TeamWorkSystem/TWS.RestApi/Models/MessageModel.cs
@@ -1,78 +1,38 @@
-<<<<<<< HEAD
 ﻿namespace TWS.RestApi.Models
-{
-    using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.Linq.Expressions;
+ {
+     using System;
+     using System.ComponentModel.DataAnnotations;
+     using System.Linq.Expressions;
 
-    using TWS.Models;
+     using TWS.Models;
 
-    public class MessageModel
-    {
-        public static Expression<Func<Message, MessageModel>> FromMessage
-        {
-            get
-            {
-                return m => new MessageModel
-                {
-                    Id = m.Id,
-                    Text = m.Text,
-                    PostDate = m.PostDate,
-                    TeamWorkId = m.TeamWorkId,
-                    SentBy = m.SentBy.UserName
-                };
-            }
-        }
+     public class MessageModel
+     {
+         public static Expression<Func<Message, MessageModel>> FromMessage
+         {
+             get
+             {
+                 return m => new MessageModel
+                 {
+                     Id = m.Id,
+                     Text = m.Text,
+                     PostDate = m.PostDate,
+                     TeamWorkId = m.TeamWorkId,
+                     SentBy = m.SentBy.UserName
+                 };
+             }
+         }
 
-        public int Id { get; set; }
+         public int Id { get; set; }
 
-        [MinLength(1)]
-        public string Text { get; set; }
+         [MinLength(1)]
+         public string Text { get; set; }
 
-        [Required]
-        public DateTime PostDate { get; set; }
+         [Required]
+         public DateTime PostDate { get; set; }
 
-        public int TeamWorkId { get; set; }
+         public int TeamWorkId { get; set; }
 
-        public string SentBy { get; set; }
-    }
-=======
-﻿namespace TWS.RestApi.Models
-{
-    using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.Linq.Expressions;
-
-    using TWS.Models;
-
-    public class MessageModel
-    {
-        public static Expression<Func<Message, MessageModel>> FromMessage
-        {
-            get
-            {
-                return m => new MessageModel
-                {
-                    Id = m.Id,
-                    Text = m.Text,
-                    PostDate = m.PostDate,
-                    TeamWorkId = m.TeamWorkId,
-                    SentById = m.SentById
-                };
-            }
-        }
-
-        public int Id { get; set; }
-
-        [MinLength(1)]
-        public string Text { get; set; }
-
-        [Required]
-        public DateTime PostDate { get; set; }
-
-        public int TeamWorkId { get; set; }
-
-        public string SentById { get; set; }
-    }
->>>>>>> 4244e651540132ff63bbfea93920ffe392eaa6fd
-}
+         public string SentBy { get; set; }
+     }
+ }
