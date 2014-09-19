117ee77c00ba9281d1d141b0c02b844e0543237f
Lyubomir Ganchev <luboganchev91@gmail.com>
2014-09-19 01:58:40 -0700

    Adding integration tests (For TeamWorkController)

---
+++ TeamWorkSystem/TWS.Services.IntergrationTests/TeamWorkControllerIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿namespace TWS.Services.IntegrationTests
+{
+	using System;
+	using System.Linq;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Telerik.JustMock;
+	using TWS.Data.Repositories;
+	using TWS.Models;
+	using System.Collections.Generic;
+	using TWS.Services.Tests;
+	using TWS.Data;
+	using System.Net;
+
+	[TestClass]
+	public class TeamWorkControllerIntegrationTests
+	{
+		[TestMethod]
+		public void GetAllWhenOneTeamWorkShouldReturnStatusCode200AndNotNullContent()
+		{
+			var mockRepository = Mock.Create<ITwsData>();
+			var models = new List<TeamWork>();
+			models.Add(Entities.GetValidTeamWork());
+
+			Mock.Arrange(() => mockRepository.TeamWorks.All())
+				.Returns(() => models.AsQueryable());
+
+			var server = new InMemoryHttpServer<TeamWork>("http://localhost/", mockRepository.TeamWorks);
+
+			var response = server.CreateGetRequest("/teamwork/all");
+
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+			Assert.IsNotNull(response.Content);
+		}
+	}
+}
---
+++ TeamWorkSystem/TWS.Services.IntergrationTests/TestTWSDependencyResolver.cs
@@ -0,0 +1,69 @@
+﻿namespace TWS.Services.IntegrationTests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Web.Http.Dependencies;
+
+	using Telerik.JustMock;
+	using TWS.Data;
+	using TWS.Data.Repositories;
+	using TWS.RestApi.Controllers;
+	using TWS.RestApi.Infrastructure;
+
+	public class TestTWSDependencyResolver<T> : IDependencyResolver where T : class
+	{
+		private IRepository<T> repository;
+
+		public IRepository<T> Repository
+		{
+			get
+			{
+				return this.repository;
+			}
+			set
+			{
+				this.repository = value;
+			}
+		}
+
+		public IDependencyScope BeginScope()
+		{
+			return this;
+		}
+
+		public object GetService(Type serviceType)
+		{
+			var repository = Mock.Create<ITwsData>();
+			var userProvider = Mock.Create<IUserIdProvider>();
+
+			if (serviceType == typeof(TeamWorkController))
+			{
+				return new TeamWorkController(repository, userProvider);
+			}
+			else if (serviceType == typeof(AssignmentController))
+			{
+				return new AssignmentController(repository);
+			}
+			else if (serviceType == typeof(RequestController))
+			{
+				return new RequestController(repository, userProvider);
+			}
+			else if (serviceType == typeof(MessageController))
+			{
+				return new MessageController(repository, userProvider);
+			}
+
+			return null;
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return new List<object>();
+		}
+
+		public void Dispose()
+		{
+
+		}
+	}
+}
